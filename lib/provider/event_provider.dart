9ae7a58c1d19fa6939feecf1be4150250a898552
WalterLimaViana <walter.lima.viana@gmail.com>
2022-06-18 23:29:06 -0300

    created function to show events on calendario page

--- lib/provider/event_provider.dart
+++ lib/provider/event_provider.dart
@@ -8,6 +8,12 @@ class EventProvider extends ChangeNotifier {
 
   List<Event> get events => _events;
 
+  DateTime _selectedDate = DateTime.now();
+
+  DateTime get selectedDate => _selectedDate;
+
+  void
+
   void addEvent(Event event) {
     _events.add(event);
 
