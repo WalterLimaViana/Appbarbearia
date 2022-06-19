6bda9725de384f14c75583bf7ac2797dff594993
WalterLimaViana <walter.lima.viana@gmail.com>
2022-06-18 23:42:59 -0300

    uptade event provider and created  a taskswidget

--- lib/pages/calendario_page.dart
+++ lib/pages/calendario_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_floating_menu/models/event_data_source.dart';
+import 'package:flutter_floating_menu/provider/event_provider.dart';
+import 'package:flutter_floating_menu/widgtes/tasks_widget.dart';
+import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class CalendarioPage extends StatefulWidget {
@@ -11,11 +15,21 @@ class CalendarioPage extends StatefulWidget {
 class _CalendarioPageState extends State<CalendarioPage> {
   @override
   Widget build(BuildContext context) {
+    final events = Provider.of<EventProvider>(context).events;
     return Scaffold(
       appBar: AppBar(title: Center(child: Text('Agendamentos'))),
       body: SfCalendar(
         view: CalendarView.month,
+        dataSource: EventDataSource(events),
         initialSelectedDate: DateTime.now(),
+        onLongPress: (details) {
+          final provider = Provider.of<EventProvider>(context, listen: true);
+          provider.setDate(details.date!);
+          showModalBottomSheet(
+            context: context,
+            builder: (context) => TaskWidget(),
+          );
+        },
       ),
     );
   }
---
+++ lib/widgtes/tasks_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskWidget extends StatefulWidget {
+  @override
+  State<TaskWidget> createState() => _TaskWidgetState();
+}
+
+class _TaskWidgetState extends State<TaskWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
